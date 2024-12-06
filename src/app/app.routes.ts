8f7ef888b6aa3c51e0a876355e50314900a86d43
Angel Guillermo <147960757+AngelGuillermo1@users.noreply.github.com>
2024-12-06 15:03:21 -0600

    Initial Configuration Routes
    Define routes to browse on components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PruebaComponent } from './components/prueba/prueba.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    {path:'prueba', component: PruebaComponent}
+];
