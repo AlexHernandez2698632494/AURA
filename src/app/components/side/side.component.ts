949fdad6f919af9533b5b99e96e75e4f8092118b
AguilaAzulIndustries <piojosevaldi@gmail.com>
2024-12-05 19:06:56 -0600

    update side menu

--- src/app/components/side/side.component.ts
+++ src/app/components/side/side.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Importar RouterModule
 
 @Component({
   selector: 'app-side',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule], // Incluir RouterModule aquí
   templateUrl: './side.component.html',
   styleUrls: ['./side.component.css'],
 })
@@ -12,17 +13,55 @@ export class SideComponent {
   isCollapsed = true; // Inicialmente está colapsada (solo íconos)
 
   menuItems = [
-    { icon: 'home', label: 'Home' },
-    { icon: 'add', label: 'Registrar' },
-    { icon: 'sensors', label: 'Sensores' },
-    { icon: 'history', label: 'Ver Historial' },
-    { icon: 'people', label: 'Usuarios' },
-    { icon: 'lock', label: 'Cambiar Contraseña' },
-    { icon: 'logout', label: 'Cerrar Sesión' },
+    { icon: 'home', label: 'Home', route: '/home', submenu: [], isSubmenuOpen: false },
+    { 
+      icon: 'add', 
+      label: 'Registrar', 
+      isSubmenuOpen: false, 
+      submenu: [
+        { label: 'Registrar Administrador', route: '/registrar/administrador' },
+        { label: 'Control Administrador', route: '/control/administrador' },
+        { label: 'Administradores Eliminados', route: '/eliminados/administradores' },
+      ]
+    },
+    { 
+      icon: 'sensors', 
+      label: 'Sensores', 
+      isSubmenuOpen: false, 
+      submenu: [
+        { label: 'Registrar Sensor', route: '/registrar/sensor' },
+        { label: 'Control Sensores', route: '/control/sensores' },
+        { label: 'Sensores Eliminados', route: '/eliminados/sensores' },
+      ]
+    },
+    { 
+      icon: 'history', 
+      label: 'Ver Historial', 
+      isSubmenuOpen: false, 
+      submenu: [
+        { label: 'Control Sesiones', route: '/control/sesiones' },
+        { label: 'Sesiones Eliminadas', route: '/eliminados/sesiones' },
+      ]
+    },
+    { 
+      icon: 'people', 
+      label: 'Usuarios', 
+      isSubmenuOpen: false, 
+      submenu: [
+        { label: 'Control Usuarios', route: '/control/usuarios' },
+      ]
+    },
+    { icon: 'lock', label: 'Cambiar Contraseña', route: '/cambiar-contraseña', submenu: [], isSubmenuOpen: false },
+    { icon: 'logout', label: 'Cerrar Sesión', route: '/logout', submenu: [], isSubmenuOpen: false },
   ];
 
-  // Alterna entre colapsar y expandir la barra lateral
   toggleSidebar() {
     this.isCollapsed = !this.isCollapsed;
+  }
+
+  toggleSubmenu(item: any) {
+    if (item.submenu) {
+      item.isSubmenuOpen = !item.isSubmenuOpen;
+    }
   }
 }
