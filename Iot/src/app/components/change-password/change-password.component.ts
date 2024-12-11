82d72a319e504df79e6279071cd52ea48a20ed89
Angel Guillermo <147960757+AngelGuillermo1@users.noreply.github.com>
2024-12-10 20:09:58 -0600

    change-password component fixed

--- Iot/src/app/components/change-password/change-password.component.ts
+++ Iot/src/app/components/change-password/change-password.component.ts
@@ -1,20 +1,43 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { SideComponent } from '../side/side.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
   standalone: true,
-  imports: [RouterOutlet, SideComponent],
+  imports: [RouterOutlet, SideComponent, FormsModule],
   templateUrl: './change-password.component.html',
-  styleUrl: './change-password.component.css'
+  styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent {
-  constructor(private router: Router){}
+  newPassword: string = '';
+  confirmNewPassword: string = '';
+  showCurrentPassword = false;
+  showNewPassword = false;
+  showConfirmNewPassword = false;
+
+  constructor(private router: Router) {}
+
   onSubmit() {
+    if (this.newPassword !== this.confirmNewPassword) {
+      alert('Las contraseñas no coinciden. Por favor, verifica.');
+      return;
+    }
     alert('Contraseña cambiada exitosamente');
   }
-  onBackClick():void{
+
+  onBackClick(): void {
     this.router.navigate(['/admin/index']);
+  }
+
+  togglePasswordVisibility(field: string): void {
+    if (field === 'current') {
+      this.showCurrentPassword = !this.showCurrentPassword;
+    } else if (field === 'new') {
+      this.showNewPassword = !this.showNewPassword;
+    } else if (field === 'confirmNew') {
+      this.showConfirmNewPassword = !this.showConfirmNewPassword;
+    }
   }
 }
