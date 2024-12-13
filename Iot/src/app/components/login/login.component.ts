8dc25e4521bb5e07ed39c0794cc9350d10c06370
AguilaAzulIndustries <piojosevaldi@gmail.com>
2024-12-13 15:30:27 -0600

    restore login

--- Iot/src/app/components/login/login.component.ts
+++ Iot/src/app/components/login/login.component.ts
@@ -1,52 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideLoginComponent } from '../side-login/side-login.component';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { AuthService } from '../../services/admin.service';
-import { Router } from '@angular/router';
+import { SideLoginComponent } from "../side-login/side-login.component";
 
 @Component({
   selector: 'app-prueba',
-  imports: [RouterOutlet, SideLoginComponent, ReactiveFormsModule],
+  imports: [RouterOutlet, SideLoginComponent],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   title = 'login';
-  loginForm: FormGroup;
-
-  constructor(
-    private fb: FormBuilder,
-    private authService: AuthService,
-    private router: Router
-  ) {
-    this.loginForm = this.fb.group({
-      username: ['', [Validators.required]],
-      password: ['', [Validators.required]]
-    });
-  }
-
-  ngOnInit(): void {}
-
-  // Método que maneja el envío del formulario
-  onSubmit(): void {
-    if (this.loginForm.valid) {
-      const { username, password } = this.loginForm.value;
-
-      // Llamar al servicio de autenticación
-      this.authService.login(username, password).subscribe({
-        next: (response) => {
-          // Guardar el token en el localStorage
-          this.authService.saveToken(response.token);
-          
-          // Redirigir a la vista principal (o cualquier vista protegida)
-          this.router.navigate(['/admin/index']);
-        },
-        error: (err) => {
-          console.error('Error de autenticación', err);
-          // Mostrar un mensaje de error en caso de fallo
-        }
-      });
-    }
-  }
 }
