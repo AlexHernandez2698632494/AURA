5a542f0e3f413016625d113bb98a0eb9c98681d2
Angel Guillermo <147960757+AngelGuillermo1@users.noreply.github.com>
2024-12-20 18:55:50 -0600

    create and design alert control view

--- IoT/src/app/components/alert/index/index.component.ts
+++ IoT/src/app/components/alert/index/index.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+import { SideComponent } from '../../side/side.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AdminService } from '../../../services/admin.service';
+import Swal from 'sweetalert2';  // Importar SweetAlert2
+import { EditAlertDialogComponent } from '../../../modals/edit-alert-dialog/edit-alert-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
-  selector: 'app-index',
-  imports: [],
+  selector: 'app-index-alert',
+  imports: [
+    RouterOutlet,
+    SideComponent,
+    FormsModule,
+    CommonModule,
+    MatTableModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexAlertComponent {
+  constructor(private dialog: MatDialog) { }
+  editAlert() {
+    const dialogRef = this.dialog.open(EditAlertDialogComponent, {
+      width: '400px'
+    });
+  }
 
+  deleteAlert() {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: `¿Estás seguro que deseas eliminar la alerta tal?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminarlo!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        Swal.fire('Eliminado!', `La alerta ha sido eliminado correctamente.`, 'success');
+      }
+    });
+  }
 }
--- IoT/src/app/modals/edit-alert-dialog/edit-alert-dialog.component.ts
+++ IoT/src/app/modals/edit-alert-dialog/edit-alert-dialog.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { AdminService } from '../../services/admin.service';
+import { Observable } from 'rxjs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import Swal from 'sweetalert2';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgxColorsModule } from 'ngx-colors';
 
 @Component({
   selector: 'app-edit-alert-dialog',
-  imports: [],
+  imports: [    
+    NgxColorsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatButtonModule,
+    MatSelectModule,],
   templateUrl: './edit-alert-dialog.component.html',
   styleUrl: './edit-alert-dialog.component.css'
 })
 export class EditAlertDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<EditAlertDialogComponent>){}
+
+    color:string = '#EC407A';
+
+  onCancel() {
+    this.dialogRef.close();
+  }
+
+  onSave(){
+    Swal.fire({
+              title: 'Éxito!',
+              text: 'El usuario se ha actualizado correctamente.',
+              icon: 'success',
+              confirmButtonText: 'Aceptar',
+            });
+            this.dialogRef.close();
+  }
 
 }
