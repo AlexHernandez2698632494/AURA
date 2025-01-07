535f3fdb62807a2f8647cc8a336bd54be33d1fa1
Angel Guillermo <147960757+AngelGuillermo1@users.noreply.github.com>
2025-01-06 19:09:29 -0600

    forgot component navbar bottom

--- Iot/src/app/components/forgot/forgot.component.ts
+++ Iot/src/app/components/forgot/forgot.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideLoginComponent } from "../side-login/side-login.component";
+import { NavComponent } from '../nav/nav.component';
 
 @Component({
   selector: 'app-forgot',
-  imports: [RouterOutlet,SideLoginComponent],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './forgot.component.html',
   styleUrl: './forgot.component.css'
 })
