f063abb01d022faec6e74208d928694dfee42fdd
AguilaAzulIndustries <133897289+AlexHernandez2698632494@users.noreply.github.com>
2025-01-27 07:55:03 -0600

    Update api-config.service.ts

--- IoT/src/app/services/ApiConfig/api-config.service.ts
+++ IoT/src/app/services/ApiConfig/api-config.service.ts
@@ -6,18 +6,19 @@ import { Injectable } from '@angular/core';
 export class ApiConfigService {
   private apiUrlLocal = 'http://localhost:3000';
   private apiUrlNetwork = 'http://192.168.1.82:3000';
-  private apiUrlnetWork2 ='http://192.168.1.14:3000';
+  private apiUrlnetWork2 = 'http://192.168.1.14:3000';
+  private apiUrlnetWork3 = 'http://172.16.100.200:3000';
 
   /**
    * Determina la URL base dependiendo del entorno.
    */
   getApiUrl(): string {
-    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.hostname === '172.16.100.200') {
       return this.apiUrlLocal;
-    }if (window.location.hostname === '192.168.1.14' ) {
+    } if (window.location.hostname === '192.168.1.14') {
       return this.apiUrlnetWork2;
-    }else{
-    return this.apiUrlNetwork;
+    } else {
+      return this.apiUrlNetwork;
     }
   }
 }
