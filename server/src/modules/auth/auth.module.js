6bf21b731e9f7655cce6b633d01834584be6025f
AguilaAzulIndustries <piojosevaldi@gmail.com>
2025-02-26 09:04:46 -0600

    Update auth.module.js

--- server/src/modules/auth/auth.module.js
+++ server/src/modules/auth/auth.module.js
@@ -3,6 +3,7 @@ import userRoutes from "./routes/users.routes.js";
 import premiumRoutes from "./routes/userPayment.routes.js";
 import authoritiesKeyRoutes from "./routes/authoritiesKey.routes.js";
 import historyRoutes from "./routes/history.routes.js";
+import devRoutes from "./routes/dev/usersDev.routes.js";
 
 const router = express.Router();
 
@@ -10,6 +11,7 @@ router.use("/oauth2",userRoutes);
 router.use("/oauth2", premiumRoutes);
 router.use("/oauth2", authoritiesKeyRoutes);
 router.use("/oauth2", historyRoutes);
+router.use("/oauth2", devRoutes);
 
 
 export default router;
