a0b1771c0da955cb1aec308a62db0157615b82c5
AguilaAzulIndustries <piojosevaldi@gmail.com>
2025-04-06 17:56:27 -0600

    Update fiware.service.ts

--- IoT/src/app/services/fiware/fiware.service.ts
+++ IoT/src/app/services/fiware/fiware.service.ts
@@ -13,7 +13,17 @@ export class FiwareService {
   constructor(private http: HttpClient, private apiConfig: ApiConfigService) {
     this.baseUrl = `${this.apiConfig.getApiUrl()}/v1/ngsi`;  // URL de la API de Fiware
   }
-
+  private getAuthHeaders(fiwareService: string): HttpHeaders {
+    const token = sessionStorage.getItem('token');
+    if (!token) {
+      throw new Error('No se encontró el token en sessionStorage');
+    }
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'fiware-service': fiwareService
+    });
+  }
+  
   // Método para obtener servicios de la ruta /v1/ngsi/services-path
   getSubServices(fiwareService: string): Observable<any> {
     const headers = new HttpHeaders({
@@ -23,7 +33,22 @@ export class FiwareService {
     return this.http.get(`${this.baseUrl}/services-path`, { headers })
       .pipe(catchError(this.handleError));
   }
+  getSubServiceBuilding(fiwareService: string): Observable<any> {
+    const headers = this.getAuthHeaders(fiwareService);
 
+    return this.http.get(`${this.baseUrl}/services-path/building`, { headers })
+      .pipe(catchError(this.handleError));
+  }
+
+  getSubServiceBranch(fiwareService: string,fiwareServicePath:string): Observable<any> {
+    const headers = new HttpHeaders({
+      'fiware-service': fiwareService,
+      'fiware-service-building': fiwareServicePath
+    });
+
+    return this.http.get(`${this.baseUrl}/services-path/branch`, { headers })
+      .pipe(catchError(this.handleError));
+  }
   // Método para obtener entidades de la ruta /v1/ngsi/entities
   getEntitiesWithAlerts(fiwareService: string, fiwareServicePath: string, type: string = '', limit: number = 100): Observable<any> {
     const headers = new HttpHeaders({
