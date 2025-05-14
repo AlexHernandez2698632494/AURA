d5d22620504c741dac715a2e3c9a275f75bacfba
AguilaAzulIndustries <piojosevaldi@gmail.com>
2025-05-14 11:00:30 -0600

    Update api-config.service.ts

--- IoT/src/app/services/ApiConfig/api-config.service.ts
+++ IoT/src/app/services/ApiConfig/api-config.service.ts
@@ -5,19 +5,24 @@ import { Injectable } from '@angular/core';
 })
 export class ApiConfigService {
   private apiUrls = [
-    'http://192.168.1.82:8080',
-    'http://10.0.12.138:8080',
-    'http://172.16.99.86:8080',
-    'http://192.168.1.25:8080',
-    'http://10.0.12.86:8080',
-    'http://172.16.100.200:8080', 
-    'http://localhost:8080',
+    //'https://lm89f4q4-8080.use.devtunnels.ms',
+    // 'http://192.168.1.82:8090',
+    // "http://10.0.12.66:8090",
+    "http://10.0.12.82:8090",
+    //  'http://localhost:8090' ,
+  ];
+  private apiUrlsMoquitto = [
+    // "http://10.0.12.66:8668",
+    "http://10.0.12.82:8668",
+    //  'http://localhost:8668' ,
   ];
 
   private selectedApiUrl: string | null = null;
+  private selectedApiUrlMoquitto: string | null = null;
 
   constructor() {
     this.checkApiUrls();
+    this.checkApiUrlsMoquitto();
   }
 
   /**
@@ -44,11 +49,35 @@ export class ApiConfigService {
       this.selectedApiUrl = this.apiUrls[0]; // Usa localhost como fallback
     }
   }
+  private async checkApiUrlsMoquitto() {
+    for (const url of this.apiUrlsMoquitto) {
+      try {
+        // Intenta hacer una petición GET a un endpoint conocido
+        const response = await fetch(`${url}/ping`, { method: 'GET' });
+
+        if (response.ok) {
+          this.selectedApiUrlMoquitto = url;
+          console.log(`API encontrada en: ${url}`);
+          break;
+        }
+      } catch (error) {
+        console.warn(`No se pudo conectar a: ${url}`);
+      }
+    }
+
+    if (!this.selectedApiUrlMoquitto) {
+      console.error('No se encontró una API disponible');
+      this.selectedApiUrlMoquitto = this.apiUrlsMoquitto[0]; // Usa localhost como fallback
+    }
+  }
 
   /**
    * Devuelve la URL de la API seleccionada.
    */
   getApiUrl(): string {
     return this.selectedApiUrl ?? this.apiUrls[0];
+  }
+  getApiUrlMoquitto(): string {
+    return this.selectedApiUrlMoquitto ?? this.apiUrlsMoquitto[0];
   }
 }
