b7272b014534c8730c0bc1899f9a1b4d2b5197ae
AguilaAzulIndustries <piojosevaldi@gmail.com>
2025-05-30 16:52:48 -0600

    Update ngsi.routes.js

--- api_aura/src/modules/fiware/routes/ngsi.routes.js
+++ api_aura/src/modules/fiware/routes/ngsi.routes.js
@@ -1,16 +1,32 @@
 import express from "express";
-import { createRule, deleteRule, getAllRules, getEntitiesWithAlerts, getRuleById, getServicePaths, getSubServiceBranch, getSubServiceBuilding, getSubServiceBuildingAndBranch, sendDataToAgent, updateActuatorStatusController, updateRule } from "../controllers/ngsi.controller.js";
-import { get } from "mongoose";
+import {
+  createRule,
+  deleteRule,
+  getAllRules,
+  getEntitiesWithAlerts,
+  getRuleById,
+  getRulesByServiceSubserviceActuatorAndCommand,
+  getRulesByServiceSubserviceAndActuator,
+  getServicePaths,
+  getSubServiceBranch,
+  getSubServiceBuilding,
+  getSubServiceBuildingAndBranch,
+  sendDataToAgent,
+  updateActuatorStatusController,
+  updateRule
+} from "../controllers/ngsi.controller.js";
 
 const router = express.Router();
 
 router.get("/entities", getEntitiesWithAlerts);
-router.get("/services-path",getServicePaths);
-router.get("/services-path/building",getSubServiceBuildingAndBranch);
-router.get("/services-path/branch",getSubServiceBranch);
-router.post('/sendData', sendDataToAgent);
-router.post('update/actuator',updateActuatorStatusController)
+router.get("/services-path", getServicePaths);
+router.get("/services-path/building", getSubServiceBuildingAndBranch);
+router.get("/services-path/branch", getSubServiceBranch);
+router.post("/sendData", sendDataToAgent);
+router.post("/update/actuator", updateActuatorStatusController);
 
+router.get("/rules/actuator/:actuatorId", getRulesByServiceSubserviceAndActuator);
+router.get("/rules/:actuatorId/:command", getRulesByServiceSubserviceActuatorAndCommand);
 router.post("/rules", createRule);
 router.get("/rules", getAllRules);
 router.get("/rules/:id", getRuleById);
